228f63605c82d6c81127e0d38ff4e20898213b6f
Oscar Escobar <qomc16@gmail.com>
2021-03-30 10:37:26 -0600

    Versión 30/03/2021

--- src/main/java/com/mycompany/proyecto_progra_v2/Empleados.java
+++ src/main/java/com/mycompany/proyecto_progra_v2/Empleados.java
@@ -1,89 +1,127 @@
 package com.mycompany.proyecto_progra_v2;
  // @author Fsociety
 public class Empleados {
-   String correo;
-   String nombre;
-   String apellido;
-   String fecha_de_nacimiento;
-   int ID;
-   int telefono;
-   String direccion;
-   String contraseña;
 
-    public Empleados(String correo, String nombre, String apellido, String fecha_de_nacimiento, int ID, int telefono, String direccion, String contraseña) {
-        this.correo = correo;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.fecha_de_nacimiento = fecha_de_nacimiento;
-        this.ID = ID;
-        this.telefono = telefono;
-        this.direccion = direccion;
-        this.contraseña = contraseña;
-    }
-
+    /**
+     * @return the correo
+     */
     public String getCorreo() {
         return correo;
     }
 
+    /**
+     * @param correo the correo to set
+     */
     public void setCorreo(String correo) {
         this.correo = correo;
     }
 
+    /**
+     * @return the nombre
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * @param nombre the nombre to set
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * @return the apellido
+     */
     public String getApellido() {
         return apellido;
     }
 
+    /**
+     * @param apellido the apellido to set
+     */
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
 
+    /**
+     * @return the fecha_de_nacimiento
+     */
     public String getFecha_de_nacimiento() {
         return fecha_de_nacimiento;
     }
 
+    /**
+     * @param fecha_de_nacimiento the fecha_de_nacimiento to set
+     */
     public void setFecha_de_nacimiento(String fecha_de_nacimiento) {
         this.fecha_de_nacimiento = fecha_de_nacimiento;
     }
 
+    /**
+     * @return the ID
+     */
     public int getID() {
         return ID;
     }
 
+    /**
+     * @param ID the ID to set
+     */
     public void setID(int ID) {
         this.ID = ID;
     }
 
+    /**
+     * @return the telefono
+     */
     public int getTelefono() {
         return telefono;
     }
 
+    /**
+     * @param telefono the telefono to set
+     */
     public void setTelefono(int telefono) {
         this.telefono = telefono;
     }
 
+    /**
+     * @return the direccion
+     */
     public String getDireccion() {
         return direccion;
     }
 
+    /**
+     * @param direccion the direccion to set
+     */
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
 
+    /**
+     * @return the contraseña
+     */
     public String getContraseña() {
         return contraseña;
     }
 
+    /**
+     * @param contraseña the contraseña to set
+     */
     public void setContraseña(String contraseña) {
         this.contraseña = contraseña;
     }
+   private String correo;
+   private String nombre;
+   private String apellido;
+   private String fecha_de_nacimiento;
+   private int ID;
+   private int telefono;
+   private String direccion;
+   private String contraseña;
+
    
    
 }
