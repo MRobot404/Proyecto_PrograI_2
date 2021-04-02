8c6fa31f1d0943cf91204bd5a63188e7a7eec36e
Oscar Escobar <qomc16@gmail.com>
2021-04-01 21:34:50 -0600

    Versión 1/04/2021 Puntos terminados:1_2

--- src/main/java/com/mycompany/proyecto_progra_v2/Main.java
+++ src/main/java/com/mycompany/proyecto_progra_v2/Main.java
@@ -3,8 +3,10 @@ package com.mycompany.proyecto_progra_v2;
 import java.util.ArrayList;
 public class Main {
 public static ArrayList<Empleados>usuarios;
+public static ArrayList<Producto>lista;
     public static void main(String[] args) {
         usuarios=new ArrayList<>();
+        lista=new ArrayList<>();
         Login log=new Login();
         log.setVisible(true);
     
---
+++ src/main/java/com/mycompany/proyecto_progra_v2/Producto.java
@@ -0,0 +1,66 @@
+package com.mycompany.proyecto_progra_v2;
+// @author MRobot404
+
+public class Producto {
+
+    /**
+     * @return the PrecioUnitario
+     */
+    public float getPrecioUnitario() {
+        return PrecioUnitario;
+    }
+
+    /**
+     * @param PrecioUnitario the PrecioUnitario to set
+     */
+    public void setPrecioUnitario(float PrecioUnitario) {
+        this.PrecioUnitario = PrecioUnitario;
+    }
+
+    /**
+     * @return the NombreProducto
+     */
+    public String getNombreProducto() {
+        return NombreProducto;
+    }
+
+    /**
+     * @param NombreProducto the NombreProducto to set
+     */
+    public void setNombreProducto(String NombreProducto) {
+        this.NombreProducto = NombreProducto;
+    }
+
+    /**
+     * @return the DescripcionProducto
+     */
+    public String getDescripcionProducto() {
+        return DescripcionProducto;
+    }
+
+    /**
+     * @param DescripcionProducto the DescripcionProducto to set
+     */
+    public void setDescripcionProducto(String DescripcionProducto) {
+        this.DescripcionProducto = DescripcionProducto;
+    }
+
+    /**
+     * @return the CantidadVenta
+     */
+    public int getCantidadVenta() {
+        return CantidadVenta;
+    }
+
+    /**
+     * @param CantidadVenta the CantidadVenta to set
+     */
+    public void setCantidadVenta(int CantidadVenta) {
+        this.CantidadVenta = CantidadVenta;
+    }
+
+    private float PrecioUnitario;
+    private String NombreProducto;
+    private String DescripcionProducto;
+    private int CantidadVenta;
+}
